39c30be7600eece33941dce91c1970ebea65a989
joemacKE <josephmuga2020@gmail.com>
2025-05-28 12:25:31 +0300

    Added the file multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 numbers = int(input("Enter a number to see its multiplication table: "))
 
-for X in range(1, 10):
-    for Y in range(1, 10):
+for X in range(1, 11):
+    for Y in range(1, 11):
         product = X * Y
         print(f"{X} X {Y} = {product}")

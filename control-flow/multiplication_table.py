e34a7512ec309fb88d96e15b5380e716e9160a5b
joemacKE <josephmuga2020@gmail.com>
2025-05-28 12:20:07 +0300

    Added the file multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-numbers = int(input("Enter a number to see its multiplication: "))
+numbers = int(input("Enter a number to see its multiplication table: "))
 
 for numbers in range(1, 10):
     for numbers in range(1, 10):

f4d6d80b2300fb9effbd7b415670a4ad30da207a
joemacKE <josephmuga2020@gmail.com>
2025-06-05 17:13:30 +0300

    Added the fns_and_dsa.py file

---
+++ control-flow/calculator.py
@@ -0,0 +1,14 @@
+a = {3,4,5,6}
+b={4,5,6,7,8,9,9,9,8}
+print(b)
+e =a.difference(b)
+print(e)
+c = a.union(b)
+print(c)
+d = a.intersection(b)
+print(d)
+
+capital_cities  = {"America": "Washington D.C", "Canada": "Ottawa", "Kenya": "Nairobi"}
+print(capital_cities["America"])
+capital_cities["India"] = "New Delhi"
+print(capital_cities)

d27d7a396a0faef0f1e8017e2f39c89abf4ec2c8
joemacKE <josephmuga2020@gmail.com>
2025-06-18 08:45:34 +0300

    Added the oop directory that contains book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,11 +5,12 @@ class Book:
         self.year = year
     
     def __str__(self):
-        return f"{self.title} by {self.author}, published in  {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"Book('{self.title!r}', '{self.author!r}', {self.year!r})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
     
     def __del__(self):
         print(f"Deleting {self.title}")
-        del self.title
+
+

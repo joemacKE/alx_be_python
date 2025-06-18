9ad5f9a3303988439f5678c0803157834bcc06ae
joemacKE <josephmuga2020@gmail.com>
2025-06-18 10:25:42 +0300

    Added the files to manage books

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,16 +2,23 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+    def __str__(self):
+        return f"{self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
+    def __str__(self):
+        return f"{self.title} by {self.author}"
 
 class Library(Book):
     def __init__(self, title, author):
@@ -19,6 +26,9 @@ class Library(Book):
         self.title = title
         self.author = author
         self.books = []
+    
+    def __str__(self):
+        return f"Library: {self.title} by {self.author}"
 
     def add_book(self, book):
         if isinstance(book, Book):
